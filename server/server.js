0c4dc8200b8c20aaa868e66b27eb7d8b67e9cbe1
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-11-06 09:48:05 +1000

    wip

--- server/server.js
+++ server/server.js
@@ -13,4 +13,12 @@ app.use(router);
 
 const delay = () => new Promise((res) => global.setTimeout(res, 1000));
 
+app.post("/save", async (req, res) => {
+  // Save
+})
+
+app.get("/load", async (req, res) => {
+  // Load
+})
+
 app.listen(5555, () => console.log("Listening on port 5555"));
