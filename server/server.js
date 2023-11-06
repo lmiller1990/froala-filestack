0a072b830d0f55510543016540eb1d6baa6e7893
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-11-06 11:21:30 +1000

    update

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import Express, { Router } from "express";
 import cors from "cors";
 import { postFile } from "./filestack.js";
 import bodyParser from "body-parser";
-import multer from "multer";
 
 const app = Express();
 app.use(cors());
@@ -13,19 +12,28 @@ app.use(router);
 
 const delay = () => new Promise((res) => global.setTimeout(res, 1000));
 
-let html = ""
+let html = "";
 
 app.post("/save", async (req, res) => {
   // Save
-  html = req.body.body
-  res.status(204)
-  res.end()
-})
+  html = req.body.body;
+  res.status(204);
+  res.end();
+});
 
 app.get("/load", async (req, res) => {
   // Load
-  await delay()
-  res.json({ html })
-})
+  await delay();
+  res.json({ html });
+});
+
+import multer from "multer";
+
+const upload = multer();
+
+router.post("/image", upload.single("file"), async (req, res) => {
+  const { link } = await postFile(req.file.buffer);
+  res.json({ link });
+});
 
 app.listen(5555, () => console.log("Listening on port 5555"));
